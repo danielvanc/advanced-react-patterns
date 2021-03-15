1ce281a3162a12c616b0d2ddb559f463aa07d661
Daniel Van Cuylenburg <email@danielvanc.com>
2021-03-15 08:54:54 +0000

    Exercise 4 - complete!

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -8,15 +8,6 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  // const togglerProps = {
-  //   on,
-  //   className: "test",
-  //   'aria-pressed': on,
-  //   onClick: toggle
-  // }
   return {on, toggle, togglerProps: {
     'aria-pressed': on,
     onClick: toggle
